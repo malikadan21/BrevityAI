9d526dc139a799cabd79a8092e8e9b989224476d
Adan Malik <malikbruh102@gmail.com>
2024-09-06 18:14:28 +0500

    Complete Project

--- src/App.jsx
+++ src/App.jsx
@@ -2,13 +2,13 @@ import { useState } from 'react'
 import './App.css'
 import { Button } from './components/ui/button'
 import { Navigate, Outlet } from 'react-router-dom'
-import { useUser } from '@clerk/clerk-react'
 import Header from './components/custom/Header'
 import { Toaster } from './components/ui/sonner'
+import { useUser } from '@clerk/clerk-react'
 
 function App() {
   const [count, setCount] = useState(0)
-  const {user,isLoaded,isSignedIn} = useUser();
+  const {isLoaded,isSignedIn} = useUser();
 
   if (isLoaded && !isSignedIn) {
     return <Navigate to={"/auth/sign-in"} />;
